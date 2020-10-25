e0700e3f9b24a3d5162c01fa97dd4ff8b26119c8
Julian Pufler <puf17640@spengergasse.at>
2020-10-25 16:30:39 +0100

    using centra to get payload and the send to server

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-const io = require('socket.io-client');
+#!/usr/bin/env node
+const io = require('socket.io-client'),
+	req = require('@aero/centra');
 
-const client = io('http://localhost:3001');
-client.on('connect', () => { console.log('connected with id:', client.id) });
-client.on('request', (data) => { client.emit('reply', data); });
-client.on('disconnect', () => { console.log('disconnected') });
+const client = io('http://localhost:5001');
+const port = process.argv[2];
+client.on('connect', () => console.log(`connected port ${port} with id: ${client.id}`));
+client.on('request', async (data) => {
+	try {
+		const request = req(`http://localhost:${port}${data.path}`, data.method).body(data.body);
+		request.reqHeaders = data.headers;
+		const response = await request.send()
+		client.emit('reply', {
+			headers: response.headers,
+			body: response.body,
+			status: response.statusCode
+		});
+	} catch(err) {
+		client.emit('error', err);
+	}
+});
+client.on('disconnect', () => console.log('disconnected'));
